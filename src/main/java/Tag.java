305345220a9400b61c66b79b716e1554dbc7d426
Chris Carr <coder.knot@gmail.com>
2017-04-05 11:08:20 -0700

    update Tag class formatting

--- src/main/java/Tag.java
+++ src/main/java/Tag.java
@@ -1,14 +1,12 @@
 import org.sql2o.*;
-import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
 
 public class Tag implements DatabaseManagement {
 
   private int id;
   private String description;
 
-  public Tag(String description) {
+  public Tag (String description) {
     this.description = description;
   }
 
--- src/test/java/TagTest.java
+++ src/test/java/TagTest.java
@@ -1,9 +1,7 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
-import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
 
 public class TagTest {
 
