5e3a05a4cc24649f081397d592c422f20de36c4a
Chris Carr <coder.knot@gmail.com>
2017-04-05 10:07:46 -0700

    fix interface name

--- src/main/java/DatabaseManagement.java
+++ src/main/java/DatabaseManagement.java
@@ -1,9 +1,9 @@
-public interface DatabaseManagment {
+public interface DatabaseManagement {
 
   public boolean equals(Object otherObject);
 
   public void save();
 
   public void delete();
-  
+
 }
